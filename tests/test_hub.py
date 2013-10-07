b6fedc25735fb7aa53a2d12a26df04154737e929
brady-vitrano <brady-vitrano@users.noreply.github.com>
2013-10-07 10:02:55 -0500

    Delete test_hub.py

--- tests/test_hub.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from mock import patch
-from dsgrid.hub import HubModel
-
-class TestHub(unittest.TestCase):
-
-    def test_can_get_grid_status(self):
-    	hub = HubModel()
-        hub.get_status()
