ee965ffae836314a243f7f146af076873663f0a6
Brady Vitrano <brady.vitrano@rackspace.com>
2013-12-31 13:32:08 -0600

    Added Dockerfile and run.sh for Google Chrome

---
+++ files/chrome/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+su chrome <<'EOF'
+Xvfb :21 -screen 0 1024x768x24 > /dev/null 2>&1 &
+export HOST_IP=$(ip addr show dev eth0 | grep "inet " | awk '{print $2}' | cut -d '/' -f 1)
+export DISPLAY=:21
+java -jar selenium-server-standalone-2.35.0.jar -role webdriver -host ${HOST_IP} -hub http://${GRID_IP}:4444/grid/register -browser browserName=chrome
+EOF
